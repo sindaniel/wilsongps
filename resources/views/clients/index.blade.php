413b1b599ced62c35afff264402d7f6a0670b4ba
Daniel <danielpunk4@gmail.com>
2021-11-28 10:38:48 -0500

    response

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -5,41 +5,41 @@
     <p>
         <a class="btn btn-success" href="{{ route('clients.create') }}">Nuevo cliente</a>
     </p>
-
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Cliente</th>
-           
-                <th scope="col">Numero</th>
-                <th></th>
-               
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($clients as $client)
-            <tr >
-                <td>{{ $client->name }}</td>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Cliente</th>
+            
+                    <th scope="col">Numero</th>
+                    <th></th>
                 
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($clients as $client)
+                <tr >
+                    <td>{{ $client->name }}</td>
+                    
 
-                <td>
-                    {{ $client->phone }} <br>
-                    <small class='text-muted'>{{ $client->type }}</small>
-                </td>
-                <td class='text-right'> 
-                    <a class="btn btn-primary" href="{{ route('clients.edit', $client) }}">Actualizar</a>    
-                    <form method="POST" onclick="return confirm('Desea eliminar este registro?');" action="{{ route('clients.destroy', $client) }}" style="display: inline-block">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger">Eliminar</button>
-                    </form>
-                </td>
-             
-              </tr>
-            @endforeach
-        </tbody>
-    </table>
-
+                    <td>
+                        {{ $client->phone }} <br>
+                        <small class='text-muted'>{{ $client->type }}</small>
+                    </td>
+                    <td class='text-right'> 
+                        <a class="btn btn-primary" href="{{ route('clients.edit', $client) }}">Actualizar</a>    
+                        <form method="POST" onclick="return confirm('Desea eliminar este registro?');" action="{{ route('clients.destroy', $client) }}" style="display: inline-block">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger">Eliminar</button>
+                        </form>
+                    </td>
+                
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
     {{ $clients->render() }}
 
